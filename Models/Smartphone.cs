a35c03dac73f0922be6a1592cbc0ee4fa7fc5134
Jana-ina21 <janainadeoliveira169@gmail.com>
2023-12-31 14:31:52 -0300

    Desafio Feito!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,9 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        public string Modelo { get; set; }
-        public string IMEI { get; set; }
-        public int Memoria { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
